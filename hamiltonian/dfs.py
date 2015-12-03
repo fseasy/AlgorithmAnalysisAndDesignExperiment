3aad091d68566973c7210ec581bb3cba7477018e
memeda <readonlyfile@hotmail.com>
2015-12-03 22:27:39 +0800

    add some

--- hamiltonian/dfs.py
+++ hamiltonian/dfs.py
@@ -1,10 +1,19 @@
 #coding=utf8
 
 import copy
+from state import StateMemoryLess as State
 
 def dfs(vertex , adj_matrix) :
     adj_matrix = copy.copy(adj_matrix) # copy , to avoid change the origin data
     stack = []
     vertex_num = len(vertex)
-    
+    # Build Root State
+    # just using convex idx 0 for the root 
+    cur_vertex_id = 0
+    previous_vertex_id = -1 
+    visited_state = [False] * vertex_num
+    root = State(cur_vertex_id , previous_vertex_id , visited_state)
+    stack.append(root)
+    while len(stack) > 0 :
+        
 
--- hamiltonian/state.py
+++ hamiltonian/state.py
@@ -3,26 +3,30 @@
 import copy
 
 
-class StateMemLess(object) :
+class StateMemoryLess(object) :
     def __init__(self , cur_vertex_id , previous_vertex_id , visited_state) :
         self.cur_vertex_id = cur_vertex_id
         self.previous_vertex_id = previous_vertex_id
-        self.visited_state = visited_state
+        self.visited_state = visited_state[:]
 
-    def get_extendable_convex_id(self , adj_matrix) :
+    def get_extendable_vertex_id(self , adj_matrix) :
         extendable_list = []
-        adj_convex_list = adj_matrix[self.cur_vertex_id]
-        for convex_id in range(len(self.visited_state)) :
+        adj_vertex_list = adj_matrix[self.cur_vertex_id]
+        for vertex_id in range(len(self.visited_state)) :
             # 1. not parent node
             # 2. adjacent
             # 3. havent been visited .
-            if ( convex_id != self.previous_id and adj_convex_list[convex_id] == True 
-                 and self.visited_state[convex_id] == False ) :
-                extendable_list.append(convex_id)
+            if ( vertex_id != self.previous_id and adj_vertex_list[vertex_id] == True 
+                 and self.visited_state[vertex_id] == False ) :
+                extendable_list.append(vertex_id)
         return extendable_list
 
-    def has_hamiltonian(self , root_idx ):
+    def has_hamiltonian(self , root_idx , adj_matrix):
         # 1. all has been visited !
-        # 2. has a way to the root convex
+        # 2. has a way to the root vertex
         is_all_visited = reduce(lambda a , b : a and b , self.visited_state)
+        if is_all_visited and adj_matrix[self.cur_vertex_id][root_idx] :
+            return True
+        else :
+            return False
 
